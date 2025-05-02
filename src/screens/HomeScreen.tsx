2fa231665ab4908b4573d3b8ea05ad53f7062eaf
doruk batur <dbatur4@hotmail.com>
2025-05-02 12:09:05 +0300

    android build test 02 may

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -85,7 +85,7 @@ const HomeScreen: React.FC<{ navigation: any }> = ({ navigation }) => {
       showsVerticalScrollIndicator={false}
     >
       <View style={styles.header}>
-        <Text style={styles.title}>Appcircle CodePush Test 29 Apr, 29Apr 15299</Text>
+        <Text style={styles.title}>Appcircle CodePush Test 30 april test Speaking club</Text>
         <Text style={styles.subtitle}>Current Version: {currentVersion}</Text>
       </View>
 
