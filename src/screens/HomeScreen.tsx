4c99e322f63e4c0ee56c31625dffaf7da1c84102
boztopuz <boztopuz@gmail.com>
2025-05-21 08:37:23 +0300

    rn-version-0.76

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -83,7 +83,7 @@ const HomeScreen: React.FC<{ navigation: any }> = ({ navigation }) => {
       showsVerticalScrollIndicator={false}
     >
       <View style={styles.header}>
-        <Text style={styles.title}>Appcircle CodePush 30 Apr Setur Test CodePush</Text>
+        <Text style={styles.title}>Appcircle CodePush RN version 0.76</Text>
         <Text style={styles.subtitle}>Current Version: {currentVersion}</Text>
 
         {updateAvailable && (
